b15d27edebf5469f6b77125d919927384d93331c
Gwennael Jean <gwennael.jean@gmail.com>
2017-09-10 19:47:47 +0200

    feat: recuperation des donnees du formulaire

--- public/index.js
+++ public/index.js
@@ -1,7 +1,18 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+jQuery('#submit-button').on("click", function () {
+    var data = {
+        enfant: jQuery('form').find('[data-name=enfant]').val(),
+        mois: jQuery('form').find('[data-name=mois]').val(),
+        jours: jQuery('form').find('[data-name=jours]').val(),
+        periodes: []
+    };
+    
+    jQuery('table').find('tbody').find('tr').each((tr) => {
+        data.periodes.push({
+            periode: jQuery(tr).find('[data-name=periode]').val(),
+            motif: jQuery(tr).find('[data-name=motif]').val(),
+        });
+    });
+    
+    console.log(data);
+});
