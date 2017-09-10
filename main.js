f853c0c3f99177fe5dd65bfcf986b7540cf0971a
Gwennael Jean <gwennael.jean@gmail.com>
2017-09-10 15:49:55 +0200

    feat: lancement de l'application

--- main.js
+++ main.js
@@ -1,7 +1,67 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+var path = require('path');
+var electron = require('electron');
 
+var PUBLIC_PATH = path.join(__dirname, 'public');
+
+var Window = {
+
+    main: null,
+
+    init: function () {
+        if (Window.main === null) {
+            Window.create();
+        }
+    },
+
+    create: function () {
+        Window.main = new electron.BrowserWindow({
+            width: 850,
+            height: 650
+        });
+
+        Window.main.setMenu(Window.getMenu());
+
+        Window.main.loadURL('file://' + PUBLIC_PATH + '/index.html');
+
+        Window.main.webContents.openDevTools();
+
+        Window.main.on('closed', function () {
+            Window.main = null;
+        });
+    },
+
+    getMenu: function () {
+        return electron.Menu.buildFromTemplate([
+            {
+                label: 'Configuration',
+                submenu: [
+                    {
+                        label: 'Profil',
+                        click: function () {
+                            Window.main.loadURL('file://' + PUBLIC_PATH + '/profil.html');
+                        }
+                    }
+                ]
+            },
+            {
+                label: 'Quitter',
+                click: function () {
+                    electron.app.quit();
+                }
+            }
+        ]);
+    }
+};
+
+electron.app.on('ready', Window.create);
+
+electron.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron.app.quit();
+    }
+});
+
+electron.app.on('activate', function () {
+    Window.init();
+});
