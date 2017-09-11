e413e51073b49364afac25a75ae85fece639849c
Jean Gwennael <gwennael.jean@trsb.net>
2017-09-11 11:49:02 +0200

    feat: generation du fichier docx

--- public/index.js
+++ public/index.js
@@ -1,18 +1,36 @@
 
+var JSZip = require('jszip');
+var fs = require('fs');
+var Docxtemplater = require('docxtemplater');
+var FileSaver = require('file-saver');
+
 jQuery('#submit-button').on("click", function () {
     var data = {
         enfant: jQuery('form').find('[data-name=enfant]').val(),
         mois: jQuery('form').find('[data-name=mois]').val(),
         jours: jQuery('form').find('[data-name=jours]').val(),
-        periodes: []
     };
-    
-    jQuery('table').find('tbody').find('tr').each((tr) => {
-        data.periodes.push({
-            periode: jQuery(tr).find('[data-name=periode]').val(),
-            motif: jQuery(tr).find('[data-name=motif]').val(),
-        });
+
+    jQuery('table').find('tbody').find('tr').each((index, tr) => {
+        data["periode_" + (index + 1)] = jQuery(tr).find('[data-name=periode]').val();
+        data["motif_" + (index + 1)] = jQuery(tr).find('[data-name=motif]').val();
     });
-    
-    console.log(data);
+
+    generateFile(data);
 });
+
+function generateFile(data) {    
+    var content = fs.readFileSync(__dirname + "/files/fiche.docx", "binary");
+    var doc = new Docxtemplater();
+    
+    var zip = new JSZip(content);
+    doc.loadZip(zip);
+    
+    doc.setData(data);
+
+    doc.render();
+    
+    var out = doc.getZip().generate({type: "blob"});
+
+    FileSaver.saveAs(out, "Fiche de présence - " + data.enfant + " - " + data.mois + ".docx");
+}
