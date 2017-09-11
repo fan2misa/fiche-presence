9a7c2698df64cb698b29d2b98b8ba8b954113461
Jean Gwennael <gwennael.jean@trsb.net>
2017-09-11 12:59:56 +0200

    feat: utilisation des données du profil + signature

--- public/index.js
+++ public/index.js
@@ -3,13 +3,28 @@ var JSZip = require('jszip');
 var fs = require('fs');
 var Docxtemplater = require('docxtemplater');
 var FileSaver = require('file-saver');
+var storage = require('electron-json-storage');
+var ImageModule = require('docxtemplater-image-module');
+
+var data = {};
+
+storage.get('profil', function (error, profil) {
+    if (error)
+        throw error;
+    
+    data = Object.assign(data, {
+        nom: profil.nom + " " + profil.prenom,
+        lieu: profil.lieu,
+        signature: profil.signature
+    });
+});
 
 jQuery('#submit-button').on("click", function () {
-    var data = {
+    data = Object.assign(data, {
         enfant: jQuery('form').find('[data-name=enfant]').val(),
         mois: jQuery('form').find('[data-name=mois]').val(),
         jours: jQuery('form').find('[data-name=jours]').val(),
-    };
+    });
 
     jQuery('table').find('tbody').find('tr').each((index, tr) => {
         data["periode_" + (index + 1)] = jQuery(tr).find('[data-name=periode]').val();
@@ -19,12 +34,29 @@ jQuery('#submit-button').on("click", function () {
     generateFile(data);
 });
 
-function generateFile(data) {    
+function generateFile(data) {
+    
+    data = Object.assign(data, {
+        date: getToday()
+    });
+    
     var content = fs.readFileSync(__dirname + "/files/fiche.docx", "binary");
     var doc = new Docxtemplater();
     
     var zip = new JSZip(content);
     doc.loadZip(zip);
+    
+    doc.attachModule(new ImageModule({
+        centered: false,
+        getImage: function (tagValue, tagName) {
+            return fs.readFileSync(tagValue,'binary');
+        },
+        getSize: function (img, tagValue, tagName) {
+            sizeOf = require('image-size');
+            var dimensions = sizeOf(tagValue);
+            return [dimensions.width, dimensions.height];
+        }
+    }));
     
     doc.setData(data);
 
@@ -33,4 +65,27 @@ function generateFile(data) {
     var out = doc.getZip().generate({type: "blob"});
 
     FileSaver.saveAs(out, "Fiche de présence - " + data.enfant + " - " + data.mois + ".docx");
+}
+
+/**
+ * 
+ * @returns {getToday.today|String|Date}
+ */
+function getToday() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    today = dd + '/' + mm + '/' + yyyy;
+
+    return today;
 }
