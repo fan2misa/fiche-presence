094b205b3f0f692273a679a95182565c3bc7c74f
Jean Gwennael <gwennael.jean@trsb.net>
2017-09-14 17:46:02 +0200

    chore: utilisation de electron-html-to (en cours)

--- public/inc/pdf.js
+++ public/inc/pdf.js
@@ -1,7 +1,31 @@
 
 var fs = require('fs');
+var convertFactory = require('electron-html-to');
+var ejs = require('ejs');
+// var pdfMaker = require('pdf-maker');
+
+var conversion = convertFactory({
+    converterPath: convertFactory.converters.PDF
+});
 
 module.exports.generate = function (data) {
 
-    
+    console.log(data);
+
+    console.log(ejs.renderFile(path.join(path.dirname(__dirname), 'files', "fiche.ejs"), data));
 }
+
+//module.exports.generate = function (data) {
+//
+//    console.log(data);
+//
+//    var template = path.join(path.dirname(__dirname), 'files', "fiche.ejs");
+//
+//    pdfMaker(template, data, 'output.pdf', {
+//        paperSize: {
+//            format: 'A4',
+//            orientation: 'portrait',
+//            border: '1cm'
+//        }
+//    });
+//}
